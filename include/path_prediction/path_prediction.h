a52876628d68bebef5f061589688b1788cabc6b9
matchey <19305109+matchey@users.noreply.github.com>
2018-06-26 09:58:26 +0900

    init

---
+++ include/path_prediction/path_prediction.h
@@ -0,0 +1,15 @@
+
+#ifndef PATH_PREDICTION_H
+#define PATH_PREDICTION_H
+
+namespace path_prediction{
+	class PathPredictor{
+		public:
+		PathPredictor();
+		~PathPredictor();
+		private:
+	};
+} // namespace path_prediction
+
+#endif
+
---
+++ src/path_prediction.cpp
@@ -0,0 +1,10 @@
+
+#include "path_prediction/path_prediction.h"
+
+namespace path_prediction{
+	PathPredictor::PathPredictor()
+	{
+	}
+
+	PathPredictor::~PathPredictor() {}
+} // namespace path_prediction
